f46c323aa9e5a1c93cc0c5c3b19e864fb12ebb1a
ArtemMarchenko16 <110462384+ArtemMarchenko16@users.noreply.github.com>
2024-02-08 11:49:35 +0200

    Initialize project using Create React App

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Simpsons from "./Components/simpsons/Simpsons";
+import Characters from "./Components/RickAndMorty/Characters";
+
+
+const App = () => {
+    return (
+        <div>
+            <Simpsons/>
+            <Characters/>
+        </div>
+    );
+};
+
+export default App;
